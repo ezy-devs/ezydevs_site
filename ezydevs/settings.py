1a28e8b21ebc9de6c0d058345c7607d2ce5aecb6
Nzorov Emmanuel <nzorovemmanuel@gmail.com>
2025-08-11 02:10:40 -0700

    added env variables

--- ezydevs/settings.py
+++ ezydevs/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!$mnsz7467s04-19vd2jr=a!@4r3@(c34=6fhp_*a4%q_eewnw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ezydevstech.com', 'www.ezydevstech.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
