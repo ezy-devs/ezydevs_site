9c2d2147135a280d2c9f2a72091379092abd9623
ezy-devs <nzorovemmanuel@gmail.com>
2025-10-09 21:02:37 -0700

    ezydevs v1.1

--- ezydevs/settings.py
+++ ezydevs/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-#from decouple import config
+from decouple import config
 
 #load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!$mnsz7467s04-19vd2jr=a!@4r3@(c34=6fhp_*a4%q_eewnw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ezydevstech.com', 'www.ezydevstech.com', '127.0.0.1', 'localhost']
 
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'ezydevs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': config("DB_NAME"),
-#        'USER': config("DB_USER"),
-#        'PASSWORD': config("DB_PASSWORD"),
-#        'HOST': config("DB_HOST", default="localhost"),
-#        'PORT': config("DB_PORT", default=5432),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST", default="localhost"),
+        'PORT': config("DB_PORT", default=5432),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
